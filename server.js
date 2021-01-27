8ebee7313a8654a0cc1253cef1f5394cef1f4ac6
Twisterevolution <twisterevolution@gmail.com>
2021-01-26 21:14:50 -0300

    modificaciones para produccion en heroku

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const nivelesController = require('./controllers/nivelesController');
 
+
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
 const app = express()
 
-
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-const port = 5090
 
-require('./routes')(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-
-
-app.listen(port, () => {
-    // sequelize.datos() 
-    console.log(`Example app listening at http://localhost:${port}`)
- 
-})
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
