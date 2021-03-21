e8eb23fa61233257233ae5b6d2b8bd394a80293e
Twisterevolution <twisterevolution@gmail.com>
2021-03-21 01:19:07 -0300

    boton de volver en el menu de asitencias

--- src/components/asistenciaComponents/menuAsistencia.vue
+++ src/components/asistenciaComponents/menuAsistencia.vue
@@ -15,6 +15,11 @@
                  <v-btn x-large color="primary" block>administracion asistencias</v-btn>  
             </v-col>
         </v-row>
+         <v-row class="text-center" justify="center">
+            <v-col md="6" >
+                 <v-btn x-large  @click="$router.go(-1)" color="warning" block outlined><v-icon left>mdi-arrow-left-circle</v-icon>volver al menu </v-btn>  
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
